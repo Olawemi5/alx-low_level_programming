f1e1a609ded8cac5a4a257bc7c72b63672e3036b
Adewemimo Charles <adewemimocharles@gmail.com>
2022-10-18 15:41:17 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 - check the code
+  *
+  *Description: prints 10 times the alphabet, in lowercase
+  */
+
+void print_alphabet_x10(void)
+{
+	char lc;
+	int rp;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		for (rp = 1; rp <= 10; rp++)
+			_putchar("rp\n");
+		_putchar(lc);
+}
