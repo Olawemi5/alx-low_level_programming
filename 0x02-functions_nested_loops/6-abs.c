f0b73cec4c337e3fadaf4eafab666142169ad829
Adewemimo Charles <adewemimocharles@gmail.com>
2022-10-18 17:00:18 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  *_abs - absolute value
+  *
+  *@c: integer to be checked
+  *
+  *Return: return absolute value.
+  */
+
+int _abs(int c)
+
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (-c);
+}
