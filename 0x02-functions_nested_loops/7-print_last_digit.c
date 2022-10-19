a7b06507aebec2aa5a0953b5bb49434b179468af
Adewemimo Charles <adewemimocharles@gmail.com>
2022-10-18 17:28:45 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  *print_last_digit - check for digit
+  *
+  *@c: integer to check
+  *
+  *Return: value of last digit
+  */
+
+int print_last_digit(int c)
+
+{
+	int dg = c % 10;
+
+	if (dg < 0)
+		dg *= -1;
+
+	_putchar (dg + '0');
+
+	return (0);
+}
