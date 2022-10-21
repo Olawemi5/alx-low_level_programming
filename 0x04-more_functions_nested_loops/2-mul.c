a0bb9d23ae45c89d999f9a2a25475361a762cee5
Adewemimo Charles <adewemimocharles@gmail.com>
2022-10-21 11:01:30 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *mul - multiplies tow integers
+  *@a: Character 1
+  *@b: Character 2
+  *Return: Always 0
+  */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_numbers - printing numbers
+  *Return: Always 0.
+  */
+
+void print_numbers(void)
+{
+	int y;
+
+	while (y >= 0; y <= 9)
+		y++;
+		_putchar(y);
+	_putchar('\n');
+
+	return (0);
+}
