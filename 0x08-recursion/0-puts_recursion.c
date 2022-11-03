06c9eb68c111a5800ae2871ecab74c9497f831e2
Adewemimo Charles <adewemimocharles@gmail.com>
2022-11-03 10:31:51 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *_puts_recursion - printing a string followed by a new line
+  *@s: character in code
+  *Return - Always 0
+  */
+
+void _puts_recursion(char *s)
+{
+	char s;
+
+	_putchar("%s, s\n");
+
+	return (0);
+}
